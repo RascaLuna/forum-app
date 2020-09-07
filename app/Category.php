560182669f54d575dfe12aac184e044097f00971
t-sasaki07 <tys.jill@gmail.com>
2020-09-07 15:14:23 +0900

    first commit

---
+++ app/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'category_name'
+    ];
+}
---
+++ app/Comment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'post_id', 'comment'
+    ];
+
+    public function user() {
+        return $this->belongsTo(\App\User::class, 'user_id');
+    }
+}
